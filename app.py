7119da92d6f97fe45534027ad5ecf856c03c6f17
jakobvarg <155798912+jakobvarg@users.noreply.github.com>
2025-01-27 09:44:56 +0000

    have created dynamic data and connected to html files used list instead of storing in database

--- app.py
+++ app.py
@@ -1,10 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS =  [
+  {
+    'id': 1,
+    'title': 'UI/UX Designer',
+    'location': 'Kochi, Panampally Nagar',
+    'salary': '30,000'
+  },
+  {
+    'id': 2,
+    'title': 'Jr. Frontend Developer',
+    'location': 'Kochi, Edapally',
+    'salary': '20,000'
+  },
+  {
+    'id': 3,
+    'title': 'React JS Developer',
+    'location': 'Kochi, Infopark',
+    'salary': '40,000'
+  },
+  {
+    'id': 4,
+    'title': 'SEO Manager',
+    'location': 'Hybrid',
+  },
+  {
+    'id': 5,
+    'title': 'Python Flask Developer',
+    'location': 'Remote',
+    'salary': '50,000'
+  }
+]
+
 @app.route("/")
 def hellow():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name="Jakob's IT Jobs")
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
