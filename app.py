33c84f39457947439eedd99b978957a5c2095132
jakobvarg <155798912+jakobvarg@users.noreply.github.com>
2025-02-04 08:38:44 +0000

    Created dynamic page for each job

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_by_id
 
 app = Flask(__name__)
 
@@ -12,6 +12,15 @@ def home():
 def list_jobs():
   JOBS = load_jobs_from_db()
   return jsonify(JOBS)
+
+@app.route("/job/<id>")
+def show_jobs_by_id(id):
+  JOB = load_job_by_id(id)
+  if JOB:
+    return render_template('jobpage.html', job=JOB, company_name="Jakob's IT Jobs")
+  else:
+    return render_template('404.html')
+    
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
--- database.py
+++ database.py
@@ -21,8 +21,30 @@ def load_jobs_from_db():
         conn.close()
 
         return results
-
     except Exception as ex:
         print(f"Database error: {ex}")
         return []
 
+def load_job_by_id(id):
+    try:
+        # Open a new connection for each request
+        conn = psycopg2.connect(conn_string)
+        cursor = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+        sql = "SELECT * FROM jobs WHERE id = %s;"
+        # below we use (id,) as it is a tuple but since we converted it to dictionary
+        # with above conn.cursor() its not an issue if we type as cursor.execute(sql, id)
+        cursor.execute(sql, (id,))
+        # use fetchone() as one record is required else will return as a list
+        results = cursor.fetchone()
+
+        # Close cursor and connection after fetching data
+        cursor.close()
+        conn.close()
+        if results:
+            return results
+        else:
+            return None
+    except Exception as ex:
+        print(f"Database error: {ex}")
+        return []
